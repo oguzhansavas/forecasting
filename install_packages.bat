1d4eecf4cb026ef3d9cf4744127d5d91f3560147
Oguzhan Savas <oguzhansavas359@gmail.com>
2025-04-22 19:54:42 +0200

    install_packages

---
+++ install_packages.bat
@@ -0,0 +1,8 @@
+@echo off
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install numpy==1.24.3
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install pandas==2.0.3
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install seaborn==0.12.2
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install matplotlib==3.7.2
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install xgboost==1.7.6
+"C:\Users\Oguzhan Savas\AppData\Local\Programs\Python\Python313\python.exe" -m pip install scikit-learn==1.3.0
+pause 
